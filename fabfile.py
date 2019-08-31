ed799e2fc8a49d680dae18f5190f8af37befcbf6
Ben <bp2@icloud.com>
2019-08-30 17:19:01 -0700

    update deployment to include auto writing of versions

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,6 @@
-from fabric import Connection
-from fabric import task
-from fabric import config
+from fabric2 import Connection
+from fabric2 import task
+from fabric2 import config
 import os
 from xml.etree import ElementTree as ET
 import uuid
@@ -9,69 +9,83 @@ import glob
 workflow_components = ['input.xml', 'binding.xml', 'flow.xml', 'result.xml', 'tool.xml']
 
 @task
-def update_workflow_xml(fab_connection, workflow_name, workflow_version, production_str=""):
+def update_workflow_xml(c, workflow_name, tool_name, workflow_version, production_str=""):
     production = production_str=="production"
 
+    local_temp_path = os.path.join("/tmp/{}_{}_{}".format(workflow_name, workflow_version, str(uuid.uuid4())))
+    c.local("mkdir -p {}".format(local_temp_path))
+
     for component in workflow_components:
-        verify_workflow_component(os.path.join(workflow_name), component)
+        rewrite_workflow_component(component, workflow_name, tool_name, workflow_version, local_temp_path)
 
     if production:
-        fab_connection.sudo("mkdir -p /ccms/workflows/{}/versions".format(workflow_name), user=fab_connection["env"]["production_user"], pty=True)
-        fab_connection.sudo("mkdir -p /ccms/workflows/{}/versions/{}".format(workflow_name, workflow_version), user=fab_connection["env"]["production_user"], pty=True)
+        c.sudo("mkdir -p /ccms/workflows/{}/versions".format(workflow_name), user=c["env"]["production_user"], pty=True)
+        c.sudo("mkdir -p /ccms/workflows/{}/versions/{}".format(workflow_name, workflow_version), user=c["env"]["production_user"], pty=True)
     else:
-        fab_connection.run("mkdir -p /ccms/workflows/{}/versions".format(workflow_name))
-        fab_connection.run("mkdir -p /ccms/workflows/{}/versions/{}".format(workflow_name, workflow_version))
+        c.run("mkdir -p /ccms/workflows/{}/versions".format(workflow_name))
+        c.run("mkdir -p /ccms/workflows/{}/versions/{}".format(workflow_name, workflow_version))
 
     for component in workflow_components:
-        update_workflow_component(fab_connection, workflow_name, component, workflow_version=workflow_version, production=production) #Explicitly adding versioned
-        update_workflow_component(fab_connection, workflow_name, component, production=production) #Adding to active default version
+        update_workflow_component(c, local_temp_path, component, workflow_version=workflow_version, production=production) #Explicitly adding versioned
+        update_workflow_component(c, local_temp_path, component, production=production) #Adding to active default version
 
 #Uploading the actual tools to the server
 @task
-def update_tools(fab_connection, workflow_name, workflow_version, production_str=""):
+def update_tools(c, workflow_name, workflow_version, production_str=""):
     production = production_str=="production"
 
     if production:
-        fab_connection.sudo("mkdir -p /data/cluster/tools/{}/{}".format(workflow_name, workflow_version), user=fab_connection["env"]["production_user"])
+        c.sudo("mkdir -p /data/cluster/tools/{}/{}".format(workflow_name, workflow_version), user=c["env"]["production_user"])
     else:
-        fab_connection.run("mkdir -p /data/cluster/tools/{}/{}".format(workflow_name, workflow_version))
+        c.run("mkdir -p /data/cluster/tools/{}/{}".format(workflow_name, workflow_version))
 
     local_path = 'tools/{}/'.format(workflow_name)
     final_path = '/data/cluster/tools/{}/{}/'.format(workflow_name, workflow_version)
 
-    update_folder(fab_connection, local_path, final_path, production=production)
-
+    update_folder(c, local_path, final_path, production=production)
 
 
 
 #Utility Functions
 
-def verify_workflow_component(workflow_filename, component):
-    local = '{}/{}'.format(workflow_filename,component)
-    xml = ET.parse(local)
+def rewrite_workflow_component(component, workflow_name, tool_name, workflow_version, local_temp_path):
+    local = os.path.join(workflow_name,component)
+    temp = os.path.join(local_temp_path,component)
+    tree = ET.parse(local)
+    root = tree.getroot()
+    if component in ['input.xml','result.xml']:
+        root.set('id', workflow_name)
+        root.set('version', workflow_version)
+    elif component in ['flow.xml']:
+        root.set('name', workflow_name)
+    elif component in ['tool.xml']:
+        for path in root.findall('pathSet'):
+            if path.attrib['base'] == '$base':
+                path.attrib['base'] = os.path.join(tool_name,workflow_version)
+    tree.write(temp)
 
 #TODO: Validate that the xml is also a valid workflow
-def update_workflow_component(fab_connection, workflow_filename, component, workflow_version=None, production=False):
-    local = '{}/{}'.format(workflow_filename,component)
+def update_workflow_component(c, workflow_filename, component, workflow_version=None, production=False):
+    local = os.path.join(workflow_filename,component)
     if workflow_version:
         server = '/ccms/workflows/{}/versions/{}/{}'.format(workflow_filename, workflow_version, component)
     else:
         server = '/ccms/workflows/{}/{}'.format(workflow_filename, component)
-    update_file(fab_connection, local, server, production=production)
+    update_file(c, local, server, production=production)
 
 
 
 #Update File
-def update_file(fab_connection, local_path, final_path, production=False):
+def update_file(c, local_path, final_path, production=False):
     if production:
         remote_temp_path = os.path.join("/tmp/{}_{}".format(local_path.replace("/", "_"), str(uuid.uuid4())))
-        fab_connection.put(local_path, remote_temp_path, preserve_mode=True)
-        fab_connection.sudo('cp {} {}'.format(remote_temp_path, final_path), user=fab_connection["env"]["production_user"], pty=True)
+        c.put(local_path, remote_temp_path, preserve_mode=True)
+        c.sudo('cp {} {}'.format(remote_temp_path, final_path), user=c["env"]["production_user"], pty=True)
     else:
-        fab_connection.put(local_path, final_path, preserve_mode=True)
+        c.put(local_path, final_path, preserve_mode=True)
 
 #TODO: update this to work with rsync
-def update_folder(fab_connection, local_path, final_path, production=False):
+def update_folder(c, local_path, final_path, production=False):
     #Tar up local folder and upload to temporary space on server and untar
     local_temp_path = os.path.join("/tmp/{}_{}.tar".format(local_path.replace("/", "_"), str(uuid.uuid4())))
     cmd = "tar -C {} -chvf {} .".format(local_path, local_temp_path)
@@ -79,14 +93,13 @@ def update_folder(fab_connection, local_path, final_path, production=False):
     os.system(cmd)
 
     remote_temp_tar_path = os.path.join("/tmp/{}_{}.tar".format(local_path.replace("/", "_"), str(uuid.uuid4())))
-    fab_connection.put(local_temp_path, remote_temp_tar_path, preserve_mode=True)
+    c.put(local_temp_path, remote_temp_tar_path, preserve_mode=True)
 
     remote_temp_path = os.path.join("/tmp/{}_{}".format(local_path.replace("/", "_"), str(uuid.uuid4())))
-    fab_connection.run("mkdir {}".format(remote_temp_path))
-    fab_connection.run("tar -C {} -xvf {}".format(remote_temp_path, remote_temp_tar_path))
+    c.run("mkdir {}".format(remote_temp_path))
+    c.run("tar -C {} -xvf {}".format(remote_temp_path, remote_temp_tar_path))
 
     if production:
-        fab_connection.sudo('rsync -rlptDv {}/ {}'.format(remote_temp_path, final_path), user=fab_connection["env"]["production_user"], pty=True)
+        c.sudo('rsync -rlptDv {}/ {}'.format(remote_temp_path, final_path), user=c["env"]["production_user"], pty=True)
     else:
-        fab_connection.run('rsync -rlptDv {}/ {}'.format(remote_temp_path, final_path))
-
+        c.run('rsync -rlptDv {}/ {}'.format(remote_temp_path, final_path))
