820cc19f090685fba1288d1042273fd69599c5c6
Ben Pullman <bp2@icloud.com>
2019-09-06 08:47:27 -0700

    update fabfile + yml to allow for bulk deployments

--- fabfile.py
+++ fabfile.py
@@ -9,47 +9,64 @@ import glob
 workflow_components = ['input.xml', 'binding.xml', 'flow.xml', 'result.xml', 'tool.xml']
 
 @task
-def update_workflow_xml(c, workflow_name, tool_name, workflow_version, production_str=""):
+def update_workflow_from_makefile(c, workflow_name):
+    params = {}
+    with open(os.path.join(workflow_name,'Makefile')) as f:
+        for l in f:
+            split_line = l.rstrip().split('=')
+            if len(split_line) == 2:
+                params[split_line[0]] = split_line[1]
+    update_workflow_xml(c, params["WORKFLOW_NAME"], params["TOOL_FOLDER_NAME"], params["WORKFLOW_VERSION"], workflow_name, "production")
+    update_tools(c, params["TOOL_FOLDER_NAME"], params["WORKFLOW_VERSION"], workflow_name, "production")
+
+@task
+def deploy_all(c):
+    if "workflows" not in c:
+        exit("Deploy all only works for production workflows.")
+    for workflow in c["workflows"]:
+        update_workflow_from_makefile(c, workflow)
+
+@task
+def update_workflow_xml(c, workflow_name, tool_name, workflow_version, base_dir=".", production_str=""):
     production = production_str=="production"
 
     local_temp_path = os.path.join("/tmp/{}_{}_{}".format(workflow_name, workflow_version, str(uuid.uuid4())))
     c.local("mkdir -p {}".format(local_temp_path))
 
     for component in workflow_components:
-        rewrite_workflow_component(component, workflow_name, tool_name, workflow_version, local_temp_path)
+        rewrite_workflow_component(component, base_dir, workflow_name, tool_name, workflow_version, local_temp_path)
 
     if production:
-        c.sudo("mkdir -p /ccms/workflows/{}/versions".format(workflow_name), user=c["env"]["production_user"], pty=True)
-        c.sudo("mkdir -p /ccms/workflows/{}/versions/{}".format(workflow_name, workflow_version), user=c["env"]["production_user"], pty=True)
+        c.sudo("mkdir -p /ccms/workflows/{}/versions".format(workflow_name), user=c["env"]["production_user"]["workflow"], pty=True)
+        c.sudo("mkdir -p /ccms/workflows/{}/versions/{}".format(workflow_name, workflow_version), user=c["env"]["production_user"]["workflow"], pty=True)
     else:
         c.run("mkdir -p /ccms/workflows/{}/versions".format(workflow_name))
         c.run("mkdir -p /ccms/workflows/{}/versions/{}".format(workflow_name, workflow_version))
 
     for component in workflow_components:
-        update_workflow_component(c, local_temp_path, workflow_name, component, workflow_version=workflow_version, production=production) #Explicitly adding versioned
-        update_workflow_component(c, local_temp_path, workflow_name, component, production=production) #Adding to active default version
+        update_workflow_component(c, local_temp_path, workflow_name, component, workflow_version=workflow_version, production=production, production_user=c["env"]["production_user"]["workflow"]) #Explicitly adding versioned
+        update_workflow_component(c, local_temp_path, workflow_name, component, production=production, production_user=c["env"]["production_user"]["workflow"]) #Adding to active default version
 
 #Uploading the actual tools to the server
 @task
-def update_tools(c, workflow_name, workflow_version, production_str=""):
+def update_tools(c, workflow_name, workflow_version, base_dir=".", production_str=""):
     production = production_str=="production"
 
     if production:
-        c.sudo("mkdir -p /data/cluster/tools/{}/{}".format(workflow_name, workflow_version), user=c["env"]["production_user"], pty=True)
+        c.sudo("mkdir -p /data/cluster/tools/{}/{}".format(workflow_name, workflow_version), user=c["env"]["production_user"]["tool"], pty=True)
     else:
         c.run("mkdir -p /data/cluster/tools/{}/{}".format(workflow_name, workflow_version))
 
-    local_path = 'tools/{}/'.format(workflow_name)
+    local_path = '{}/tools/{}/'.format(base_dir, workflow_name)
     final_path = '/data/cluster/tools/{}/{}/'.format(workflow_name, workflow_version)
 
-    update_folder(c, local_path, final_path, production=production)
-
+    update_folder(c, local_path, final_path, production=production, production_user=c["env"]["production_user"]["tool"])
 
 
 #Utility Functions
 
-def rewrite_workflow_component(component, workflow_name, tool_name, workflow_version, local_temp_path):
-    local = os.path.join(workflow_name,component)
+def rewrite_workflow_component(component, base_dir, workflow_name, tool_name, workflow_version, local_temp_path):
+    local = os.path.join(base_dir, workflow_name,component)
     temp = os.path.join(local_temp_path,component)
     tree = ET.parse(local)
     root = tree.getroot()
@@ -76,16 +93,16 @@ def update_workflow_component(c, local_temp_path, workflow_filename, component, 
 
 
 #Update File
-def update_file(c, local_path, final_path, production=False):
+def update_file(c, local_path, final_path, production=False, production_user=None):
     if production:
         remote_temp_path = os.path.join("/tmp/{}_{}".format(local_path.replace("/", "_"), str(uuid.uuid4())))
         c.put(local_path, remote_temp_path, preserve_mode=True)
-        c.sudo('cp {} {}'.format(remote_temp_path, final_path), user=c["env"]["production_user"], pty=True)
+        c.sudo('cp {} {}'.format(remote_temp_path, final_path), user=production_user, pty=True)
     else:
         c.put(local_path, final_path, preserve_mode=True)
 
 #TODO: update this to work with rsync
-def update_folder(c, local_path, final_path, production=False):
+def update_folder(c, local_path, final_path, production=False, production_user=None):
     #Tar up local folder and upload to temporary space on server and untar
     local_temp_path = os.path.join("/tmp/{}_{}.tar".format(local_path.replace("/", "_"), str(uuid.uuid4())))
     cmd = "tar -C {} -chvf {} .".format(local_path, local_temp_path)
@@ -100,6 +117,6 @@ def update_folder(c, local_path, final_path, production=False):
     c.run("tar -C {} -xvf {}".format(remote_temp_path, remote_temp_tar_path))
 
     if production:
-        c.sudo('rsync -rlptDv {}/ {}'.format(remote_temp_path, final_path), user=c["env"]["production_user"], pty=True)
+        c.sudo('rsync -rlptDv {}/ {}'.format(remote_temp_path, final_path), user=production_user, pty=True)
     else:
         c.run('rsync -rlptDv {}/ {}'.format(remote_temp_path, final_path))
